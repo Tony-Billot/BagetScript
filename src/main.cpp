320b262fd250cb8cec5ec4d660af8d9f6aa84e06
Tony-Billot <100514277+Tony-Billot@users.noreply.github.com>
2026-08-29 15:54:28 +0200

    refactor: restart project from scratch with cleaner architecture

--- src/main.cpp
+++ src/main.cpp
@@ -1,123 +1,29 @@
 #include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <sstream>
 #include <string>
-#include <filesystem>
+#include <windows.h>
 
-#include "interpreter.hpp"
-#include "lexer.hpp"
-#include "parser.hpp"
-#include "token.hpp"
-
-namespace
+int main()
 {
-std::string escapeForDisplay(const std::string& value)
-{
-    std::string out;
+    // Activer accents et caractères UTF8
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
 
-    for (char c : value)
+    std::cout << "BagetScript v0.1\n";
+    std::cout << "Tapez 'quitter' pour fermer cette fenêtre.\n\n";
+
+    std::string input;
+
+    while (true)
     {
-        if (c == '\n')
-            out += "\\n";
-        else if (c == '\t')
-            out += "\\t";
-        else if (c == '\r')
-            out += "\\r";
-        else
-            out += c;
-    }
+        std::cout << ">>> ";
 
-    return out;
-}
+        if (!std::getline(std::cin, input))
+            break;
 
-void printTokens(const std::vector<Token>& tokens)
-{
-    std::cout << "\n===== TOKENS =====\n";
-    std::cout << std::left
-              << std::setw(6) << "Ligne"
-              << std::setw(18) << "Type"
-              << "Lexeme"
-              << '\n';
-    std::cout << std::string(50, '-') << '\n';
+        if (input == "quitter")
+            break;
 
-    for (const auto& token : tokens)
-    {
-        std::string lexeme = escapeForDisplay(token.value);
-
-        if (token.type == TokenType::STRING)
-            lexeme = "\"" + lexeme + "\"";
-
-        std::cout << std::left
-                  << std::setw(6) << token.line
-                  << std::setw(18) << tokenTypeToString(token.type)
-                  << lexeme
-                  << '\n';
-    }
-
-    std::cout << std::endl;
-}
-}
-
-int main(int argc, char** argv)
-{
-    if (argc < 2)
-    {
-        std::cout << "Commande à utiliser : bagetscript <ton_fichier>.bgt [--dstokens]" << std::endl;
-        return 1;
-    }
-
-    std::filesystem::path path(argv[1]);
-
-    // Vérifie que le fichier possède bien l'extension .bgt
-    if (path.extension() != ".bgt")
-    {
-        std::cerr << "Erreur : le fichier " << path << " n'a pas l'extension .bgt" << std::endl;
-        return 1;
-    }
-
-    bool debugTokens = false;
-
-    if (argc >= 3)
-    {
-        std::string option = argv[2];
-
-        if (option == "--dstokens")
-        {
-            debugTokens = true;
-        }
-    }
-
-    std::ifstream file(path);
-    if (!file.is_open())
-    {
-        std::cerr << "Erreur : impossible d'ouvrir le fichier " << path << std::endl;
-        return 1;
-    }
-
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-
-    std::string code = buffer.str();
-
-    try
-    {
-        Lexer lexer(code);
-        std::vector<Token> tokens = lexer.tokenize();
-
-        if (debugTokens)
-            printTokens(tokens);
-
-        Parser parser(tokens);
-        auto program = parser.parse();
-
-        Interpreter interpreter;
-        interpreter.execute(*program);
-    }
-    catch (const std::exception& e)
-    {
-        std::cerr << "Erreur : " << e.what() << std::endl;
-        return 1;
+        std::cout << "Vous avez rentré : " << input << '\n';
     }
 
     return 0;
