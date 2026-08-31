7c7f23c32ea0c88bed688c1571bf88851582bcb3
Tony Billot <100514277+Tony-Billot@users.noreply.github.com>
2026-08-31 15:14:39 +0200

    fix: add file extension security

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,22 @@ int main(int argc, char* argv[])
     if (argc > 1)
     {
         std::ifstream file(argv[1]);
+        std::string extension = argv[1];
+        if (extension.find_last_of('.') != std::string::npos)
+        {
+            extension = extension.substr(extension.find_last_of('.'));
+        }
+        else
+        {
+            extension = "";
+        }   
+
+        if (extension != ".bgt")
+        {
+            std::cerr << "Le fichier doit être un fichier bagetscript (.bgt).\n";
+            return 1;
+        }
+
         if (!file.is_open())
         {
             std::cerr << "Impossible d'ouvrir le fichier : " << argv[1] << '\n';
